8218719c838e3d6ce8d45c01c9cda4c1aeddb922
Kaciade <valerypolyakov60@gmail.com>
2023-05-08 22:47:56 +0200

    Задание 24 изменено

--- Task_24.py
+++ Task_24.py
@@ -13,4 +13,4 @@ arr_count = list()
 for i in range(len(arr) - 1):
     arr_count.append(arr[i - 1] + arr[i] + arr[i + 1])
 arr_count.append(arr[-2] + arr[-1] + arr[0])
-print(max(arr_count))
+print(f"Максимальное число ягод, которое может собрать за один заход собирающий модуль = {max(arr_count)}")
